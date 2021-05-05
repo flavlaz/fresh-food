3d0c21fc61b8fd74bc59c80a05f6a2707065a810
flavlaz <flaviu_lazar@gmail.com>
2021-05-05 23:49:47 +0300

    add files

---
+++ script.js
@@ -0,0 +1,110 @@
+/*=== MENU TOGGLE ===*/
+
+const toggleMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.add('show-menu');
+        })
+    }
+}
+toggleMenu('nav-toggle', 'nav-menu');
+
+
+/*=== REMOVE MENU TOGGLE ===*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+/*=== SCROLL SECTIONS ACTIVE LINK ===*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive(){
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current=>{
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
+
+
+/*=== CHANGE BACLGROUND HEADER ===*/
+function scrollHeader(){
+    const nav = document.getElementById('header');
+
+    //when the scroll is greater than 200 viewport height, add the scroll-header class to the header
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader);
+
+
+/*=== SHOW SCROLL TOP ===*/
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+
+    //when the scroll is higher than 500 viewport height, add the show-scroll class to the scrollTop
+    if(this.scrollY >= 500) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop);
+
+
+/*==== CHANGE THEME ====*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark':'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon':'bx-sun';
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+/*==== SCROLL REVEAL ====*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: false
+});
+
+sr.reveal(`.home__data, .home__img,
+            .about__data, .about__img,
+            .services__content, .menu__content,
+            .app__data, .app__img,
+            .contact__data, .contact__button,
+            .footer__content`,{
+    interval: 200
+})
